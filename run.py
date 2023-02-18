5d688acbd49cf4666cd6769aa79cc8d5bd71eeb5
cristiandramu <christian.dramu@gmail.com>
2023-02-18 01:05:15 +0000

    write function to create the battleships

--- run.py
+++ run.py
@@ -1,41 +1,40 @@
-#Random module for randomly accepting the values
-# ‘X’ indicates the ships hit
+
+# ‘*’ indicates the ships hit
 # ‘-‘ indicates the hits missed
 from random import randint
 
-Hidden_Pattern=[[' ']*8 for x in range(8)]
-Guess_Pattern=[[' ']*8 for x in range(8)]
+Hidden_Pattern = [[' '] * 8 for x in range(8)]
+Guess_Pattern = [[' '] * 8 for x in range(8)]
 
-let_to_num={'A':0,'B':1, 'C':2,'D':3,'E':4,'F':5,'G':6,'H':7}
+let_to_num = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
+
 
 def print_board(board):
     print('  A  B  C  D  E  F  G  H')
     print(' ________________________')
-    row_num=1
+    row_num = 1
     for row in board:
         print("%d|%s|" % (row_num, "|".join(row)))
-        row_num +=1
+        row_num += 1
 
-def print_board(board):
-    print(' A B C D E F G H')
-    print(' ***************')
-    row_num=1
-    for row in board:
-        print("%d|%s|" % (row_num, "|".join(row)))
-        row_num +=1
-
+# Enter the row number between 1 to 8
 def get_ship_location():
-    
-    #Enter the row number between 1 to 8
-    row=input('Please enter a ship row 1-8 ').upper()
+    row = input('Please enter a ship row 1-8 ').upper()
     while row not in '1 to 8 range':
         print("Please enter a valid row ")
-        row=input('Please enter a ship row 1-8 ')
-    
-    #Enter the Ship column from A to H
-    column=input('Please enter a ship column A-H ').upper()
+        row = input('Please enter a ship row 1-8 ')
+# Enter the Ship column from A to H
+    column = input('Please enter a ship column A-H ').upper()
     while column not in 'A B C D E F G H':
         print("Please enter a valid column ")
-        column=input('Please enter a ship column A-H ')
-    return int(row)-1,let_to_num[column]
+        column = input('Please enter a ship column A-H ')
+    return int(row)-1, let_to_num[column]
 
+
+# Function to create the battleships
+def create_ships(board):
+    for ship in range(5):
+        ship_r, ship_cl = randint(0, 7), randint(0, 7)
+        while board[ship_r][ship_cl] == "*":
+            ship_r, ship_cl = randint(0, 7), randint(0, 7)
+        board[ship_r][ship_cl] = '*'
